50889996a1feac779623954a0a8368f53fc14e2c
VidaZing <VidaZing@gmail.com>
2018-08-25 22:03:39 -0700

    Fixing many rubocop issues

--- test/test_vidazing_logger.rb
+++ test/test_vidazing_logger.rb
@@ -32,23 +32,3 @@ class TestNAME < Test::Unit::TestCase
     FileUtils.rm_rf(LOG_DIR)
   end
 end
-
-# describe Thing do
-#   describe '.add_task' do
-#     let(:repo) { Thing.repo }
-
-#     it 'adds a new task' do
-#       expect { Thing.add_task('speak @ Guru-SP') }.to change{ repo.count }.by(1)
-#     end
-
-#     context 'when title is blank' do
-#       subject(:add_blank_task) { Thing.add_task('') }
-
-#       it 'does not persist' do
-#         expect { add_blank_task }.to_not change{ repo.count }
-#       end
-
-#       it { eq(false) }
-#     end
-#   end
-# end
